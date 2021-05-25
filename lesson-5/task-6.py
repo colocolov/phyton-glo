d7318fac0128fd76ec784e61c76bfd34048541cc
colocolov <colocolov.stas@gmail.com>
2021-05-25 16:51:35 +0300

    Add new file

--- lesson-5/task-6.py
+++ lesson-5/task-6.py
@@ -3,8 +3,6 @@ number = int(input())
 
 digit_one = number % 2
 
-#print(digit_one)
-
 if number > 0 and number <= 36:
     if (number >= 1 and number <= 10) or (number >= 19 and number <= 28):
         if digit_one == 0:
---
+++ lesson-6/task-1.py
@@ -0,0 +1,8 @@
+print('Введите пароль:')
+pass_one = input()
+print('Введите пароль повторно:')
+pass_two = input()
+if pass_one == pass_two:
+    print('Все окей!')
+else:
+    print('Пароли не совпадают!')
