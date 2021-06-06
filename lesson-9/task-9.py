97920b8bdb974170761208410a90151ad390f576
Colocolov Stas <colocolov.stas@gmail.com>
2021-06-06 20:19:48 +0300

    Fix task-9 in lesson 9

--- lesson-9/task-9.py
+++ lesson-9/task-9.py
@@ -1,9 +1,9 @@
-print('Ð’Ð²ÐµÐ´Ð¸Ñ‚Ðµ Ñ‡Ð¸ÑÐ»Ð¾:')
+print('Ââåäèòå ÷èñëî:')
 number = int(input())
 
-quantity = 0
 summa = 0
 total = 0
+total2 = 0
 
 while number != 0:
     last_digit = number % 10
@@ -14,5 +14,11 @@ while summa != 0:
     last_digit = summa % 10
     total = total + last_digit
     summa = summa // 10
+    if total > 10:
+        while total != 0:
+            last_digit = total % 10
+            total2 = total2 + last_digit
+            total = total // 10
+        total = total2
 
-print(summ)
+print(total)
