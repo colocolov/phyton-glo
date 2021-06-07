ffd6026330bcf7d0c0a6f6a0a762c25f2d676719
Colocolov Stas <colocolov.stas@gmail.com>
2021-06-08 00:09:42 +0300

    Fix task-9 in lesson 9

--- lesson-9/task-9.py
+++ lesson-9/task-9.py
@@ -3,22 +3,22 @@ number = int(input())
 
 summa = 0
 total = 0
-total2 = 0
 
 while number != 0:
     last_digit = number % 10
     summa = summa + last_digit
     number = number // 10
-
-while summa != 0:
-    last_digit = summa % 10
-    total = total + last_digit
-    summa = summa // 10
-    if total > 10:
-        while total != 0:
-            last_digit = total % 10
-            total2 = total2 + last_digit
-            total = total // 10
-        total = total2
+    if number == 0:
+        while summa != 0:
+            last_digit = summa % 10
+            total = total + last_digit
+            summa = summa // 10
+            if total > 9:
+                summa = total
+                total = 0
+                while summa != 0:
+                    last_digit = summa % 10
+                    total = total + last_digit
+                    summa = summa // 10
 
 print(total)
